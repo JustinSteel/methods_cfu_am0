689bb6ff2d685584141e757dfc63a38ff4f7535d
Justin Lee <justinslee201100@yahoo.com>
2023-08-04 12:41:04 -0600

    Answer built_in_methods.rb

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -10,19 +10,31 @@
 
 
 "Hello World".include?("Hello")
+# The include method is called on the string object "Hello World" 
+# No arguments are passed; include is asking if "Hello" is included in the string "Hello World"
+# The return value is true
 
 
 "Hello World".end_with?("Hello")
-
+# The end_with method is called on the string object "Hello World" 
+# No arguments are passed; end_with is asking if "Hello" is at the end of the string "Hello World"
+# The return value is false
 
 "Hello World".end_with?("rld")
+# The end_with method is called on the string object "Hello World" 
+# No arguments are passed; end_with is asking if "rld" is at the end of the string "Hello World"
+# The return value is true because in world the rld is at the end.
 
 
 12.even?
-
+# the even method is called on the integer 12
+# No arguments are passed; even is asking if the integer 12 is divisible by 2 or is an even number
+# The return value is true 
 
 18.next
-
+# The next method is called on the integer 18
+# No arguments are passsed; next is asking what the next integer is after 18 
+# The return value is 19
 
 
 # SECTION 2: Calling methods on variables assigned to strings.
@@ -46,11 +58,20 @@ puts first_name.start_with?("J")
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Integer.html
 # Include comments above each method call explaining the impact and return value of that method.
-
-
+potatos = 12
+puts potatos.gcd(144)
+# The .gcd is findinding the greates common divisible number between the variable potatos which is equal to 12 
+#and the argument 144
+# The return value is 12 because 12 is the largest number that can both numbers can be devided with and still 
+#produce a whole number.
+# The puts command prints the return balue of the .gcd method (12) to the console
 
 # SECTION 4: Calling methods on variables assigned to arrays.
 # Declare 2 variables assigned to arrays.
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Array.html
 # Include comments above each method call explaining the impact and return value of that method.
+burgers = [1, 2, 3, 4, 5]
+puts burgers.sum
+# The .sum method is being used on the "burgers" array which adds all of the numbers in the array
+# there is no argument; and the return value will show 15 on the console
