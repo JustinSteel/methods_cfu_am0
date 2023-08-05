8876010c25e2ea44979bdfeada23ee30cab6cf74
Justin Lee <justinslee201100@yahoo.com>
2023-08-05 14:54:24 -0600

    answer final_practice.rb questions

--- final_practice.rb
+++ final_practice.rb
@@ -5,31 +5,49 @@
 
 
 # 1: Write a method named greeting that returns a string with a general greeting. 
-
+def greeting
+    puts "Hola"
+end
 # What is the return value of your method?
+Hola
 # How many arguments did you pass your method?
-
+I dont have any arguments in this one
 
 # 2: Write a method named custom_greeting that returns a greeting WITH a specific name.
-
+def custom_greeting(name)
+    puts "Howdy #{name}, sure is a hot one"
+end
 # What is the return value of your method?
+Howdy "Justin", sure is a hot one
 # How many arguments did you pass your method?
+i passed one arguent (name)
 # What data type was your argument(s)?
-
+String
 
 # 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
-
+def greet_person(first, middle, last)
+    puts "#{first} #{middle} #{last} GET OVRE HERE!"
+end
 # What is the return value of your method?
+"Justin Steele Lee GET OVER HERE!"
 # How many arguments did you pass your method?
+3 first, middle, last
 # What data type was your argument(s)?
-
+String
 
 # 4: Write a method named square that takes in one integer, and returns the square of that integer.
 # Bonus: Print a sentence that interpolates the return value of your square method.
+def square(num)
+    puts "square is #{num * num} "
+        
+end
 
 # What is the return value of your method?
+# If you put square 5 it will return 25
 # How many arguments did you pass your method?
+#there is 1 argument
 # What data type was your argument(s)?
+#integer
 
 
 # 5: Write a method named check_stock that satisfies the following interaction pattern:
@@ -46,3 +64,14 @@ check_stock(0, "Cheese");
 
 check_stock(1, "Salsa");
 # => "Salsa - running LOW"
+def check_stock(num)
+    if num == 4
+        puts "Coffee is stocked"
+    elsif num == 3
+        puts "Tortillas - running LOW"
+    elsif num == 0 
+        puts "Cheese - OUT of stock!"
+    elsif num == 1
+        puts "Salsa - running LOW"
+    end
+end
